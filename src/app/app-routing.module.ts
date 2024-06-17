700a5b14cb30348ab20bdf6a7f0b56b519fc4283
JhoanJ <jhoanf3001@gmail.com>
2024-06-17 18:58:25 -0500

    se inicio el frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './public/pages/home/home.component';
+import { LoginComponent } from './public/pages/login/login.component';
+import { RegisterComponent } from './public/pages/register/register.component';
+import { PetsComponent } from './private/pages/pets/pets.component';
+import { BookingsComponent } from './private/pages/bookings/bookings.component';
+import { OurServicesComponent } from './private/pages/our-services/our-services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component: HomeComponent},
+  {path:"login",component: LoginComponent},
+  {path:"register", component: RegisterComponent},
+  {path:"dashboard/pets", component: PetsComponent},
+  {path:"dashboard/bookings", component: BookingsComponent},
+  {path:"dashboard/our-services", component: OurServicesComponent},
+  {path:"**", redirectTo: ""}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { PetsComponent } from './private/pages/pets/pets.component';
+import { OurServicesComponent } from './private/pages/our-services/our-services.component';
+import { BookingsComponent } from './private/pages/bookings/bookings.component';
+import { HomeComponent } from './public/pages/home/home.component';
+import { LoginComponent } from './public/pages/login/login.component';
+import { RegisterComponent } from './public/pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PetsComponent,
+    OurServicesComponent,
+    BookingsComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
